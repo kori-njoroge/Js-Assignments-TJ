3b92dd9ada0af0d43465dc40150ad54d1074fdd5
Kori Njoroge <korinjoroge63@gmail.com>
2023-01-20 08:24:21 +0300

    Even + Array

---
+++ EvenOrOdd/index.js
@@ -0,0 +1,12 @@
+// alert("yooo")
+
+
+function input(num){
+    if(num%2 === 0){
+        console.log(`${num} is Even`)
+    }else{
+        console.log(`${num} is Odd`)
+    }
+}
+
+input(10)
---
+++ LastElementOfArray/index.js
@@ -0,0 +1,10 @@
+// alert("ssup")
+
+let myArray = ["StarWars","AquaMan","Batman","HarleyQuinn","Atrocitus","Bane","TheBeetle"]
+
+function getLastEl(array){
+    console.log(`The last element is ${array[array.length -1]}`)
+    console.log(`Number of items in the array = ${array.length}`)
+}
+
+getLastEl(myArray)
